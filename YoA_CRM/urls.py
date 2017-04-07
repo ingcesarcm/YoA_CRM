0ff9eb951574979dc4af750c003963d6602b5524
cesarcm <cesarcm@CESARCRUZMATA>
2017-04-07 13:04:24 -0500

    Add project files.

---
+++ YoA_CRM/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for YoA_CRM.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', YoA_CRM.views.home, name='home'),
+    # url(r'^YoA_CRM/', include('YoA_CRM.YoA_CRM.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
